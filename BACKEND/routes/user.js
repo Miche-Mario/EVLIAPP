23c21361a8bd4133cb104bb9061d855f21b0df09
Miche <gmespace.74@gmail.com>
2022-06-21 06:44:08 -0700

    fourth commit

--- BACKEND/routes/user.js
+++ BACKEND/routes/user.js
@@ -4,8 +4,11 @@ const router = express.Router();
 
 
 const jwt = require("jsonwebtoken");
-const { eventNames } = require("..");
+const nodemailer= require("nodemailer");
 require('dotenv').config();
+var auth = require("../services/authentification")
+var checkRole = require('../services/checkRole');
+const { authentificateToken } = require("../services/authentification");
 
 
 router.post('/signup', (req, res) => {
@@ -55,6 +58,92 @@ router.post('/login', (req, res) => {
     })
 })
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+})
+
+router.post('/forgotpassword',(req,res) => {
+    const user = req.body;
+    query = "select email, password from user where email=?";
+    connection.query(query,[user.email],(err, results)=> {
+        if(!err){
+            if(results.length <= 0){
+                return res.status(200).json({message: "Password sent successfully to your email."})
+            }else {
+                var mailOptions= {
+                    from: process.env.EMAIL,
+                    to: results[0].email,
+                    subject: 'Password by EVLI SMS',
+                    html: '<p><br>Your login details for EVLI SMS</b><br><b>Email: </b>'+results[0].email+'<br><b>Password: </b>'+results[0].password+'<br><a href="http://localhost:4200">Click here to login</a></p>'
+                };
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error){
+                        console.log(error)
+                    }else {
+                        console.log('Email sent: '+info.response)
+                    }
+                });
+                return res.status(200).json({message: "Password sen successfully to your email"})
+            }
+        }else {
+            return res.status(500).json(err)
+        }
+    })
+});
+
+router.get('/users',authentificateToken,checkRole.checkRole, (req, res) => {
+    var query = "select id, name,email, contactNumber,status from user where role='user'";
+    connection.query(query, (err, results)=> {
+        if(!err){
+            return res.status(200).json(results);
+        }else {
+            return res.status(500).json(err);
+        }
+    })
+});
+
+
+router.patch('/update',authentificateToken,checkRole.checkRole, (req,res) => {
+    let user = req.body;
+    var query = "update user set status=? where id=?";
+    connection.query(query,[user.status, user.id], (err, results) => {
+        if(!err) {
+            if(results.affectedRows == 0) {
+                return res.status(404).json({ùessage: "User id doesn't exist"});
+            }
+            return res.status(200).json({message: "User updated successfully"});
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+router.get('checkToken',authentificateToken, (req, res) => {
+    return res.status(200).json({message: "true"});
+})
+
+router.post('/changePassword', (req, res) => {
+    const user =req.body;
+    const email = res.locals.email;
+    var query = "select * from user where email=? and password=?";
+    connection.query(query, [email, user.oldPassword], (err, results) => {
+        if(!err) {
+            if(results.length <= 0) {
+                return res.status(400).json({message: "Incorrect Old Password"});
+            } else if(results[0].password == user.oldPassword) {
+
+            } else {
+                return res.status(400).json({message: "Something went wrong"})
+            }
+        } else {
+            return res.status
+        }
+    })
+})
 
 
 
