f17439e772f8f75075a6290c2d9ac85224ab435f
Miche Mario <micheganhou@gmail.com>
2022-07-27 07:37:37 +0000

    cc

--- table.sql
+++ table.sql
@@ -1,32 +1,90 @@
-create table user(
-    id int primary key AUTO_INCREMENT,
-    name varchar(250),
-    contactNumber varchar(20),
-    email varchar(50),
-    password varchar(250),
-    status varchar(20),
-    role varchar(20),
-    UNIQUE (email)
+----------------------------------------------------COURSES
+CREATE TABLE courses(
+    courseid INT PRIMARY KEY AUTO_INCREMENT,
+    coursename VARCHAR(250),
+    courseprice INTEGER,
+    hastype BOOLEAN
 );
-
-insert into user(name, contactNumber, email, password, status, role)
-values('Admin', '12365','admin@gmail.com', 'admin', 'true', 'admin');
-
-
-create table category(
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    primary key(id)
+CREATE TABLE coursetype(
+    coursetypeid INT PRIMARY KEY AUTO_INCREMENT,
+    type1 VARCHAR(250),
+    type2 VARCHAR(250),
+    type3 VARCHAR(250),
+    type4 VARCHAR(250),
+    type5 VARCHAR(250),
+    courseid INT,
+    FOREIGN KEY (courseid) REFERENCES courses(courseid)
 )
+--------------------------------------------------------------------------------ACCOMODATIONS
 
-
-create table product(
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    categoryId integer NOT NULL,
-    description varchar(255),
-    price integer,
-    status varchar(20),
-    primary key(id)
-
+create table accomodations(
+    accomodationid INT PRIMARY KEY AUTO_INCREMENT,
+    accomodationname VARCHAR(250),
+    accomodationprice INTEGER,
+    hastype BOOLEAN
 );
+CREATE TABLE coursetype(
+    accomodationtypeid INT PRIMARY KEY AUTO_INCREMENT,
+    type1 VARCHAR(250),
+    type2 VARCHAR(250),
+    type3 VARCHAR(250),
+    type4 VARCHAR(250),
+    type5 VARCHAR(250),
+    accomodationid INT,
+    FOREIGN KEY (accomodationid) REFERENCES accomodations(accomodationid)
+)
+--------------------------------------------------------------------------------EXAMS
+
+CREATE TABLE exams(
+    examid INT PRIMARY AUTO_INCREMENT,
+    examname VARCHAR(250),
+    examprice INTEGER
+);
+
+--------------------------------------------------------------------------------PURCHASES
+
+CREATE TABLE purchases(
+    purchaseid int primary key AUTO_INCREMENT,
+    purchasename varchar(250),
+    purchaseprice integer,
+    purchasedescription
+);
+
+------------------------------------------------------------------------------------------------STUDENTS
+
+CREATE TABLE student_seq
+(
+  studentid INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+-------------------------------------------------------------------------------------------------
+CREATE TABLE students
+(
+  studentid VARCHAR(7) NOT NULL PRIMARY KEY DEFAULT '0', 
+  surnameg VARCHAR(250),
+  forenamesg VARCHAR(250),
+  genderg VARCHAR(250),
+  dateofbirthg DATE,
+  placeofbirthg VARCHAR(250),
+  citizenshipg VARCHAR(250),
+  occupationg VARCHAR(250),
+  emailg VARCHAR(250),
+  telhomeg VARCHAR(250),
+  telghanag VARCHAR(250),
+  maritalg VARCHAR(250),
+  passportidg VARCHAR(250),
+  academicg VARCHAR(250),
+  noteg VARCHAR(250),
+  aboutidg int,
+  passportphotographg 
+);
+-------------------------------------------------------------------------------------------------
+DELIMITER $$
+CREATE TRIGGER tg_student_insert
+BEFORE INSERT ON students
+FOR EACH ROW
+BEGIN
+  INSERT INTO student_seq VALUES (NULL);
+  SET NEW.id = CONCAT('EVLI-', LPAD(LAST_INSERT_ID(), 5, '0'));
+END$$
+DELIMITER ;
